7eb1e02f43de99e3e8651edb3188e6983573c7f1
RaianiYohana <raianirescarolli@gmail.com>
2024-10-24 10:25:10 -0300

    importações

---
+++ trabalho4/App.js
@@ -0,0 +1,4 @@
+import { useState, useEffect } from 'react';
+import { Platform, Text, View, StyleSheet } from 'react-native';
+import * as Location from 'expo-location';
+import axios from 'axios'; 
